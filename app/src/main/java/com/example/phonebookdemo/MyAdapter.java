92a103bf69c113dc0a969718d36a1fe5ead4c2e0
Dell <mollanoman2019@gmail.com>
2020-03-01 16:54:49 +0600

    PhoneBookDemo-v2

--- app/src/main/java/com/example/phonebookdemo/MyAdapter.java
+++ app/src/main/java/com/example/phonebookdemo/MyAdapter.java
@@ -1,15 +1,20 @@
 package com.example.phonebookdemo;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -23,10 +28,10 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     List<Model_Class> arrayList;
 
 
-    public MyAdapter(Context context, List<Model_Class>arrayList,MyClick myClick) {
-        this.context=context;
-        this.arrayList=arrayList;
-        this.myClick=myClick;
+    public MyAdapter(Context context, List<Model_Class> arrayList, MyClick myClick) {
+        this.context = context;
+        this.arrayList = arrayList;
+        this.myClick = myClick;
 
     }
 
@@ -34,25 +39,52 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-      View view=  LayoutInflater.from(context).inflate(R.layout.sample,parent,false);
-      final MyViewHolder myViewHolder=new MyViewHolder(view);
-     view.setOnLongClickListener(new View.OnLongClickListener() {
-         @Override
-         public boolean onLongClick(View view) {
-             myClick.OnMyClick(view,myViewHolder.getPosition());
-             return true;
-         }
-     });
+        View view = LayoutInflater.from(context).inflate(R.layout.sample, parent, false);
+        final MyViewHolder myViewHolder = new MyViewHolder(view);
+
+
+        view.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                myClick.OnMyClick(view, myViewHolder.getPosition());
+                return true;
+            }
+        });
         return myViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
 
-       // holder.textView.setText(datalist.get(position).getCountryNames());
-          holder.circleImageView.setImageResource(arrayList.get(position).getPhoto());
-          holder.nameTextView.setText(arrayList.get(position).getName());
-          holder.phoneTextView.setText(String.valueOf(arrayList.get(position).getPhone()));
+        // holder.textView.setText(datalist.get(position).getCountryNames());
+        holder.circleImageView.setImageResource(arrayList.get(position).getPhoto());
+        holder.nameTextView.setText(arrayList.get(position).getName());
+        holder.phoneTextView.setText(arrayList.get(position).getPhone());
+        holder.phoneImageView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                String number = arrayList.get(position).getPhone();
+                intent.setData(Uri.parse("tel:"+number));
+
+                context.startActivity(intent);
+
+
+              }
+          });
+
+        holder.smsImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                String number = arrayList.get(position).getPhone();
+                intent.setData(Uri.parse("sms:"+number));
+                String message="It's my sms";
+                intent.putExtra("sms_body",message);
+                context.startActivity(intent);
+            }
+        });
 
     }
 
